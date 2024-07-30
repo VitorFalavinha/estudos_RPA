0385a2b1a9be47fbfc1a8cc08cbdbe1ae83f050c
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2024-07-30 13:10:34 -0300

    Integração com excel e visualizações detalhadas
    O código adiciona os novos dados coletados. O problema do numero de visualizações genéricas foi resolvido com o pyautogui

--- auto_dashboard.py
+++ auto_dashboard.py
@@ -4,44 +4,81 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pyautogui as pyto
+import re
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from datetime import datetime
 
-def extract_views_comments():
+def get_data_from_url():
     # Configuração do driver do Selenium
     chrome_options = Options()
-    chrome_options.headless = False  # Mudar para True se quiser rodar sem interface gráfica
-    service = Service('chromedriver.exe')  
-    
+    chrome_options.headless = True  # Mudar para True se quiser rodar sem interface gráfica
+
+    service = Service('C:/Users/vitor/Documents/Estudos_RPA/estudos_RPA/chromedriver.exe') 
+
     # Inicializar o driver do Selenium
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # URL do vídeo do YouTube
-    url = 'https://www.youtube.com/watch?v=HQ2VYbedE1g'  # Substitua pelo ID do vídeo
-
+    url = 'https://www.youtube.com/watch?v=HQ2VYbedE1g' 
+     
     # Abrir a página do vídeo
     driver.get(url)
+    driver.maximize_window()
 
     try:
         # Espera explícita para o elemento estar presente e visível
-        views_element = WebDriverWait(driver, 20).until(
+        views_element = WebDriverWait(driver, 60).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[4]/div[1]/div/ytd-watch-info-text/div/yt-formatted-string/span[1]'))
         )
     
         # Obter o texto das visualizações e comentários
         views = views_element.text
-        print(f"O vídeo tem {views}.")
+        #print(f"O vídeo tem {views}.")
 
     except Exception as e:
         print(f"Erro ao encontrar o elemento: {e}")
 
+    pyto.PAUSE = 7
+
+    # Obtém as dimensões da tela
+    screen_width, screen_height = pyto.size()
+
+    # Calcula o ponto central da tela
+    center_x = screen_width / 2
+    center_y = screen_height / 2
+
+    # Move o mouse para o centro da tela
+    pyto.moveTo(center_x, center_y)
+
+   
+    #scroll down para visualizar comentários
+    pyto.vscroll(-500) 
+    # Clique no centro da tela para capturar as visualizações detalhadas
+    pyto.click()
+
     try:
         # Espera explícita para o elemento estar presente e visível
-        comment_element = WebDriverWait(driver, 30).until(
+        comments_element = WebDriverWait(driver, 40).until(
             EC.visibility_of_element_located((By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/div[1]'))
         )
     
         # Obter o texto das visualizações e comentários
-        comments = comment_element.text
-        print(f"O vídeo tem {comments}.")
+        comments = comments_element.text
+        #print(f"O vídeo tem {comments}.")
+
+        # Extraindo apenas os números usando expressão regular
+        views_number = int(re.sub(r'[^\d]', '', views_element.text))
+        comments_number = int(re.sub(r'[^\d]', '', comments_element.text))
+        
+        print(f"Número de visualizações: {views_number}")
+        print(f"Número de comentários: {comments_number}")
+
+        # Obtendo a data e hora atuais
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        fill_in_excel(url, views_number, comments_number, current_datetime )
 
     except Exception as e:
         print(f"Erro ao encontrar o elemento: {e}")
@@ -49,3 +86,26 @@ def extract_views_comments():
     finally:
         # Fechar o navegador
         driver.quit()
+    
+    return (views_number, comments_number)
+
+def fill_in_excel(url, views_number, comments_number, current_datetime):
+    # Criação e preenchimento da planilha Excel
+    #wb = Workbook()
+    #ws = wb.active
+    #ws.title = "YouTube_Data"
+
+    # Carregando a planilha existente
+    wb = load_workbook("C:/Users/vitor/Documents/Estudos_RPA/estudos_RPA/data.xlsx")
+    ws = wb['YouTube_Data']  
+
+    # Escrevendo os cabeçalhos
+    #ws.append(["URL", "Visualizações", "Comentários","Data da atualização"])
+
+    # Adicionando uma nova linha com os dados
+    ws.append([url, views_number, comments_number, current_datetime])
+    
+    # Salvando a planilha com os novos dados
+    wb.save("C:/Users/vitor/Documents/Estudos_RPA/estudos_RPA/data.xlsx")
+
+get_data_from_url()
---
+++ execute_script.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python C:\Users\vitor\Documents\Estudos_RPA\estudos_RPA\auto_dashboard.py
+
+python C:\Users\vitor\Documents\Estudos_RPA\estudos_RPA\auto_dashboard.py >C:\Users\vitor\Documents\Estudos_RPA\estudos_RPA\meu_log.txt 2>&1
